0fdb90c9e6b307e5e72df7533a225459ea4001f6
usama imran <usama.imran@frizhub.com>
2023-08-22 16:24:29 +0500

    change note.user

--- src/features/notes/Note.js
+++ src/features/notes/Note.js
@@ -4,16 +4,10 @@ import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { selectNoteById } from "./noteApiSlice";
-import { selectUserById } from "../users/userApiSlice";
 
 const Note = ({ noteId }) => {
   const navigate = useNavigate();
   const note = useSelector((state) => selectNoteById(state, noteId));
-  //   console.log(note.user);
-  //   const userId = note.user;
-  //   console.log(userId);
-  //   const user = useSelector((state) => selectUserById(state, userId));
-  //   console.log(user);
   if (note) {
     const handleEdit = () => navigate(`/dash/notes/${noteId}`);
     const created = new Date().toLocaleString("en-US", {
@@ -24,12 +18,6 @@ const Note = ({ noteId }) => {
       day: "numeric",
       month: "long",
     });
-    // let owner;
-    // if (user) {
-    //   owner = user.username;
-    // } else {
-    //   owner = "Owner Not Found";
-    // }
     return (
       <tr>
         <td className="table__cell note__status">
@@ -42,7 +30,7 @@ const Note = ({ noteId }) => {
         <td className="table__cell note__created">{created}</td>
         <td className="table__cell note__updated">{updated}</td>
         <td className="table__cell note__title">{note.title}</td>
-        <td className="table__cell note__username">{note.user}</td>
+        <td className="table__cell note__username">{note.user.username}</td>
         <td className="table__cell">
           <button className="icon-button table__button" onClick={handleEdit}>
             <FontAwesomeIcon icon={faPenToSquare} />
