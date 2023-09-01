61f8580a54cae93c9883d2874bae877f2318f8d0
usama imran <usama.imran@frizhub.com>
2023-09-02 01:02:21 +0500

    add authSlice and authApiSlice, update store

---
+++ src/features/auth/authSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const authSlice = createSlice({
+  name: "auth",
+  reducers: {
+    setCredentials: (state, actions) => {
+      const { accessToken } = actions.payload;
+      state.token = accessToken;
+    },
+    logOut: (state, actions) => {
+      state.token = null;
+    },
+  },
+});
+
+export default authSlice.reducer;
+
+export const { setCredentials, logOut } = authSlice.actions;
+
+export const selectCurrentToken = (state) => {
+  state.auth.token;
+};
